e0554d022b11b56ea9ec4a8d59264f068472d1b3
xudong7 <13433126742@163.com>
2024-05-24 17:26:46 +0800

    add music button

--- input.js
+++ input.js
@@ -1,6 +1,7 @@
 export default class InputHandler {
     constructor() {
         this.keys = [];
+        this.musicButton = document.getElementById('musicButton');
         window.addEventListener('keydown', (e) => {
             if ((   e.key === 'ArrowLeft' ||
                     e.key === 'ArrowRight' ||
@@ -17,6 +18,17 @@ export default class InputHandler {
                     e.key === 'ArrowDown' ||
                     e.key === 'Enter')
                 this.keys.splice(e.key);        
+        });
+        this.musicButton.addEventListener('click', function() {
+            const bgm = document.getElementById('backgroundMusic');
+            if (bgm.paused) {
+                bgm.play();
+                this.innerHTML = 'Stop Music';
+            }
+            else {
+                bgm.pause();
+                this.innerHTML = 'Play Music';
+            }
         });
     }
 }
