95bff75575206ce83bb1beb9a25e85b1f42790d3
xudong7 <13433126742@163.com>
2024-05-25 22:46:08 +0800

    add ui

--- ui.js
+++ ui.js
@@ -3,6 +3,7 @@ export class UI {
         this.game = game;
         this.fontSize = 20;
         this.fontFamily = 'consolas';
+        this.heartsImage = document.getElementById('heart');
     }
     draw(context) {
         context.font = this.fontSize + 'px ' + this.fontFamily;
@@ -10,5 +11,8 @@ export class UI {
         context.fillStyle = this.game.fontColor;
         // score
         context.fillText('Score: ' + this.game.score, 10, 30);
+        // hearts
+        for (let i = 0; i < this.game.hearts; i++)
+            context.drawImage(this.heartsImage, 18 * i + 8, 50, 16 * 1.25, 16 * 1.25);
     }
 }
